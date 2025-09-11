d38c7a6cb785124cfa091e33f13d8ebc02a97dd4
Lorin <lorin@northarroresearch.com>
2025-09-11 08:58:13 -0700

    getting started on command line execution (not yet working)

--- execution-scripts/fargate-rpt-igo-project.sh
+++ execution-scripts/fargate-rpt-igo-project.sh
@@ -20,6 +20,15 @@ df -h
 
 try() {
 
-  rpt-igo-project
+  # Activate venv (assumes it was created in Docker build or earlier)
+  source /app/.venv/bin/activate 
+
+  rpt-igo-project \
+     "/usr/lib/x86_64-linux-gnu/mod_spatialite.so" \
+     "{env:DATA_ROOT}/athena_to_rme2" \
+     "/mnt/c/nardata/work/rme_extraction/20250827-rkymtn/physio_rky_mtn_system_4326.geojson" \
+     "Physio Rky Mtn System"
+
+  # python3 -m rpt-igo-project
 
 }
