08323f0c4e0aa2aa8b5163d05f4df4f974f16cf0
Lorin <lorin@northarroresearch.com>
2025-09-11 10:26:45 -0700

    updating launcher script

--- execution-scripts/fargate-rpt-igo-project.sh
+++ execution-scripts/fargate-rpt-igo-project.sh
@@ -2,6 +2,16 @@
 # Set -e will cause the script to exit if any command fails
 # Set -u will cause the script to exit if any variable is not set
 set -eu
+# Set IFS to newline and tab to prevent word splitting issues and improve script safety
+IFS=$'\n\t'
+
+# Environment variables 
+(: "${REPORT_ID?}")
+(: "${USER_ID?}")
+(: "${SPATIALITE_PATH?}")
+
+# Turn off the set -u option once we've checked all the mandatory variables
+set +u
 
 cat<<EOF
 
@@ -15,20 +25,35 @@ cat<<EOF
                                                    
 EOF
 
+echo "REPORT_ID: $REPORT_ID"
+echo "USER_ID: $USER_ID"
+echo "SPATIALITE_PATH: $SPATIALITE_PATH"
+
+
 echo "======================  Initial Disk space usage ======================="
 df -h
 
 try() {
 
   # Activate venv (assumes it was created in Docker build or earlier)
+  uv sync
   source /app/.venv/bin/activate 
 
+  # TODO: download inputs 
+
   rpt-igo-project \
-     "/usr/lib/x86_64-linux-gnu/mod_spatialite.so" \
+     $SPATIALITE_PATH \
      "{env:DATA_ROOT}/athena_to_rme2" \
      "/mnt/c/nardata/work/rme_extraction/20250827-rkymtn/physio_rky_mtn_system_4326.geojson" \
      "Physio Rky Mtn System"
 
-  # python3 -m rpt-igo-project
+  # Future Enhancement - upload incomplete reports to a debug service instead
+  if [[ $? != 0 ]]; then return 1; fi
+
+  echo "======================  Final Disk space usage ======================="
+  df -h
+
+  echo "======================  Upload to the report server (S3) ======================"
+  # TODO: upload outputs
 
 }
