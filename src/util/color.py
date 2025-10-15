22c43ceab85fd23155bd73c9f40a6dce54d70dd5
Jagmeet Dhillon <jagmeetdhillon@Jagmeets-MacBook-Air.local>
2025-10-15 14:31:36 -0700

    Consistent fcode colors

---
+++ src/util/color.py
@@ -0,0 +1,7 @@
+from typing import Dict
+# Default, consistent colors for flow types (fcode_desc)
+DEFAULT_FCODE_COLOR_MAP: Dict[str, str] = {
+    "Artificial Path":             "#636EFA",
+    "Intermittent Stream / River": "#EF553B",
+    "Perennial Stream / River":    "#00CC96",
+}
