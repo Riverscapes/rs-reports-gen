4ded99b9fc911007f1101d6e2d3f5fff54a9a1c8
Matt Reimer <matt@northarrowresearch.com>
2025-11-18 10:38:02 -0800

    adding in a fargate script

---
+++ scripts/automation/fargate-rpt-stream-names.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# Abort on errors (-e) and unset variables (-u)
+set -eu
+IFS=$'\n\t'
+
+# Required environment variables
+(: "${REPORT_ID?}")
+(: "${USER_ID?}")
+(: "${API_TOKEN?}")
+(: "${UNIT_SYSTEM?}")
+NO_UI=true
+
+STAGE=${STAGE:-STAGING}
+
+cat <<'EOF'
+ __      __                .___ _________ .__                   .___
+/  \    /  \___________  __| _/ \_   ___ \|  |   ____  __ __  __| _/
+\   \/\/   /  _ \_  __ \/ __ |  /    \  \/|  |  /  _ \|  |  \/ __ | 
+ \        (  <_> )  | \/ /_/ |  \     \___|  |_(  <_> )  |  / /_/ | 
+  \__/\  / \____/|__|  \____ |   \______  /____/\____/|____/\____ | 
+       \/                   \/          \/                       \/                                                                                                                                                                                                                                                                                                                                                                                                                        
+EOF
+
+echo "REPORT_ID: $REPORT_ID"
+echo "USER_ID: $USER_ID"
+echo "RSReports STAGE: $STAGE"
+echo "Unit System: $UNIT_SYSTEM"
+
+
+echo "======================  Initial Disk space usage ======================="
+df -h
+
+WORK_ROOT="/usr/local/data"
+INPUTS_DIR="$WORK_ROOT/inputs"
+OUTPUTS_DIR="$WORK_ROOT/output"
+PYTHONPATH="/usr/local/rs-reports-gen/src:${PYTHONPATH:-}"
+
+uv sync
+source /usr/local/rs-reports-gen/.venv/bin/activate
+
+try() {
+  echo "======================  Downloading inputs ======================="
+  python -m api.downloadInputs \
+    "$INPUTS_DIR" \
+    --user-id "$USER_ID" \
+    --report-id "$REPORT_ID" \
+    --stage "$STAGE"
+  if [[ $? != 0 ]]; then return 1; fi
+
+  # Extract the "name" property from the $INPUTS_DIR/inputs/index.json file
+  REPORT_NAME=$(python3 -c "import json,sys; print(json.load(sys.stdin).get('name',None))" < "$INPUTS_DIR/index.json")
+  if [[ -z "$REPORT_NAME" || "$REPORT_NAME" == "null" ]]; then
+    echo "Error: Report name not found in $INPUTS_DIR/index.json"
+    return 1
+  fi
+
+  echo "======================  Running rpt-stream-names ======================="
+  python -m reports.rpt_stream_names.main \
+    "$OUTPUTS_DIR" \
+    "$INPUTS_DIR/input.geojson" \
+    "$REPORT_NAME" \
+    --include_pdf \
+    --unit_system "$UNIT_SYSTEM"
+  if [[ $? != 0 ]]; then return 1; fi
+
+  echo "======================  Zipping up ======================="
+  # Add everything in the outputs directory to a zip file inside the outputs directory
+  (cd "$OUTPUTS_DIR" && zip -r "report.zip" . --exclude "report.zip")
+  if [[ $? != 0 ]]; then return 1; fi
+  
+  echo "======================  Uploading outputs ======================="
+  python -m api.uploadOutputs \
+    "$OUTPUTS_DIR" \
+    --user-id "$USER_ID" \
+    --report-id "$REPORT_ID" \
+    --stage "$STAGE"
+  if [[ $? != 0 ]]; then return 1; fi
+
+  echo "======================  Final Disk space usage ======================="
+  df -h
+
+
+  # Cleanup
+  echo "<<PROCESS COMPLETE>>\n\n"
+
+}
+try || {
+  # On error, upload logs only
+  python -m api.uploadOutputs \
+    "$OUTPUTS_DIR" \
+    --user-id "$USER_ID" \
+    --report-id "$REPORT_ID" \
+    --stage "$STAGE" \
+    --log-only  
+  # Emergency Cleanup
+  echo "<<REPORT PROCESS ENDED WITH AN ERROR>>\n\n"
+  exit 1
+}
