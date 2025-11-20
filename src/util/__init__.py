dec3ef176cc44d488642ddc40a8911e2f1799670
Lorin Gaertner <lorin@northarrowresearch.com>
2025-11-19 16:37:59 -0800

    remove unused function that didn't even perform well

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -3,10 +3,9 @@
 from .csvhelper import est_rows_for_csv_file
 from .rs_geo_helpers import (
     get_bounds_from_gdf,
-    get_bounds_from_gpkg,
     prepare_gdf_for_athena,
     simplify_gdf,
-    simplify_to_size,
+    simplify_gdf_to_size,
 )
 from .math_functions import (
     round_down,
@@ -16,10 +15,9 @@ from .math_functions import (
 __all__ = [
     "est_rows_for_csv_file",
     "get_bounds_from_gdf",
-    "get_bounds_from_gpkg",
     "prepare_gdf_for_athena",
     "simplify_gdf",
-    "simplify_to_size",
+    "simplify_gdf_to_size",
     "round_up",
     "round_down"
 ]
