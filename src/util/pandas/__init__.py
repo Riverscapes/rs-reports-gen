c6130ea9d59ead8b6e618984d45b27eeaf5a4375
Lorin Gaertner <lorin@northarrowresearch.com>
2026-04-11 15:27:54 -0700

    ver0.1.5 working (manual intersection of dams and other layers= download to GPKG, ixn in qgis, load from gpkg)

--- src/util/pandas/__init__.py
+++ src/util/pandas/__init__.py
@@ -1,11 +1,10 @@
-from util.pandas.RSGeoDataFrame import RSGeoDataFrame
-from util.pandas.RSFieldMeta import RSFieldMeta, FieldMetaValues
-
 from util.pandas.pandas_utilities import (
     load_gdf_from_csv,
     load_gdf_from_pq,
     pprint_df_meta,
 )
+from util.pandas.RSFieldMeta import FieldMetaValues, RSFieldMeta, ureg
+from util.pandas.RSGeoDataFrame import RSGeoDataFrame
 
 __all__ = [
     "RSGeoDataFrame",
@@ -14,4 +13,5 @@ __all__ = [
     "load_gdf_from_csv",
     "load_gdf_from_pq",
     "pprint_df_meta",
+    "ureg",
 ]
