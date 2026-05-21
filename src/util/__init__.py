db1fbdd678be455ba906b99134acb58fc97f90bd
Lorin Gaertner <lorin@northarrowresearch.com>
2026-05-20 17:52:18 -0700

    common functions for all reports (set up argparse, logging etc.)

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -3,6 +3,15 @@
 from .attains_assessment import query_attains_assessments
 from .csvhelper import est_rows_for_csv_file
 from .math_functions import round_down, round_up
+from .report_entrypoint import (
+    build_common_launch_args,
+    build_output_path,
+    build_report_parser,
+    derive_report_name,
+    init_report_logging,
+    parse_report_args,
+    report_main_wrapper,
+)
 from .rs_geo_helpers import (
     get_bounds_from_gdf,
     prepare_gdf_for_athena,
@@ -11,12 +20,19 @@ from .rs_geo_helpers import (
 )
 
 __all__ = [
+    "build_common_launch_args",
+    "build_output_path",
+    "build_report_parser",
+    "derive_report_name",
     "est_rows_for_csv_file",
     "get_bounds_from_gdf",
+    "init_report_logging",
+    "parse_report_args",
     "prepare_gdf_for_athena",
     "query_attains_assessments",
-    "round_up",
+    "report_main_wrapper",
     "round_down",
+    "round_up",
     "simplify_gdf",
     "simplify_gdf_to_size",
 ]
