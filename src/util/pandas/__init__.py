db8b0e4c4ee0d72c04c54db6f36fe27ba479785f
Lorin Gaertner <lorin@northarrowresearch.com>
2026-06-08 11:19:10 -0700

    allow cache/load of metadata for better low-bandwidth testing

--- src/util/pandas/__init__.py
+++ src/util/pandas/__init__.py
@@ -1,7 +1,9 @@
 from util.pandas.pandas_utilities import (
     load_gdf_from_csv,
     load_gdf_from_pq,
+    load_meta_from_file,
     pprint_df_meta,
+    save_meta_to_file,
 )
 from util.pandas.RSFieldMeta import FieldMetaValues, RSFieldMeta, ureg
 from util.pandas.RSGeoDataFrame import RSGeoDataFrame
@@ -12,6 +14,8 @@ __all__ = [
     "FieldMetaValues",
     "load_gdf_from_csv",
     "load_gdf_from_pq",
+    "load_meta_from_file",
+    "save_meta_to_file",
     "pprint_df_meta",
     "ureg",
 ]
