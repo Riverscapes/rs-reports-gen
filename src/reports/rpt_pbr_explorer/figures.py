fe7894d8185a3b50f69e18d07750d57eb8353622
Lorin Gaertner <lorin@northarrowresearch.com>
2026-07-14 13:52:14 -0700

    add point map as new shared and use in PBR report

--- src/reports/rpt_pbr_explorer/figures.py
+++ src/reports/rpt_pbr_explorer/figures.py
@@ -2,10 +2,17 @@ import geopandas as gpd
 import pandas as pd
 import plotly.graph_objects as go
 
-from util.figures import make_aoi_outline_map
+from util.figures import make_point_map_with_aoi, point_df_to_gdf
 
 
 def build_pbr_figures(main_data: pd.DataFrame, aoi: gpd.GeoDataFrame) -> dict[str, go.Figure]:
     """returns: dictionary of figures for PBR report"""
-    figures = {"map": make_aoi_outline_map(aoi)}
+    figures = {
+        "map": make_point_map_with_aoi(
+            point_df_to_gdf(main_data, 'location.latitude', 'location.longitude'),
+            aoi,
+            hover_cols=["name"],
+            point_name="PBR Projects",
+        )
+    }
     return figures
