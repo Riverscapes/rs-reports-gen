bfd561978b53cb99effc60c9a50ac041c4496806
Jordan Gilbert <jtgilbert89@gmail.com>
2026-07-30 16:58:38 +0000

    updated figs with colors

--- src/reports/rpt_beaver_restoration_potential/figures.py
+++ src/reports/rpt_beaver_restoration_potential/figures.py
@@ -74,6 +74,7 @@ def main_statistics(df: pd.DataFrame | gpd.GeoDataFrame) -> dict[str, pint.Quant
 
     subset_df = RSGeoDataFrame(df) if isinstance(df, gpd.GeoDataFrame) else pd.DataFrame(df)
     perennial = subset_df[subset_df["fcode"].isin([46006, 55800])]
+    high_rp = subset_df[subset_df["brat_opportunity"].isin(['Conservation/Appropriate for Translocation', 'Encourage Beaver Expansion/Colonization'])]
 
     historic_dam_capacity = perennial.apply(lambda row: row["brat_hist_capacity"] * row["centerline_length"], axis=1).sum()
     total_dam_capacity = perennial.apply(lambda row: row["brat_capacity"] * row["centerline_length"], axis=1).sum()
@@ -81,29 +82,17 @@ def main_statistics(df: pd.DataFrame | gpd.GeoDataFrame) -> dict[str, pint.Quant
     realized_capacity = ((total_dams / total_dam_capacity) * ureg.dimensionless).to("percent") if total_dam_capacity > 0 else 0 * ureg.percent
     remaining_capacity = total_dam_capacity - total_dams if total_dam_capacity > 0 else 0
 
-    stats = {'historic_dam_capacity': historic_dam_capacity, 'total_dam_capacity': total_dam_capacity, 'total_dams': total_dams, 'realized_capacity': realized_capacity, 'remaining_capacity': remaining_capacity}
-
-    return stats
-
-
-def high_rp_statistics(df: pd.DataFrame | gpd.GeoDataFrame) -> dict[str, pint.Quantity]:
-    """Calculate and return key statistics for high restoration potential areas as a dictionary
-    Args:
-        df (DataFrame | GeoDataFrame): data_df input WITH UNITS APPLIED
-
-    Returns:
-        dict[str, pint.Quantity]: new summary statistics applicable to the whole dataframe
-    """
-
-    subset_df = RSGeoDataFrame(df) if isinstance(df, gpd.GeoDataFrame) else pd.DataFrame(df)
-    high_rp = subset_df[subset_df["brat_opportunity"].isin(['Conservation/Appropriate for Translocation', 'Encourage Beaver Expansion/Colonization'])]
-
     total_high_rp_capacity = high_rp.apply(lambda row: row["brat_capacity"] * row["centerline_length"], axis=1).sum()
     total_high_rp_dams = high_rp["dam_ct"].sum()
     realized_high_rp_capacity = ((total_high_rp_dams / total_high_rp_capacity) * ureg.dimensionless).to("percent") if total_high_rp_capacity > 0 else 0 * ureg.percent
     remaining_high_rp_capacity = total_high_rp_capacity - total_high_rp_dams if total_high_rp_capacity > 0 else 0
 
     stats = {
+        'historic_dam_capacity': historic_dam_capacity,
+        'total_dam_capacity': total_dam_capacity,
+        'total_dams': total_dams,
+        'realized_capacity': realized_capacity,
+        'remaining_capacity': remaining_capacity,
         'total_dam_capacity_(actionable_opportunity)': total_high_rp_capacity,
         'total_dams_(actionable_opportunity)': total_high_rp_dams,
         'realized_capacity_(actionable_opportunity)': realized_high_rp_capacity,
