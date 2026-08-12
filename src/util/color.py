30fbc685a4e98f62ea3b100d903ed10481c6c015
Jordan Gilbert <jtgilbert89@gmail.com>
2026-08-12 21:32:18 +0000

    figure colors

--- src/util/color.py
+++ src/util/color.py
@@ -1,7 +1,32 @@
-
 # Default, consistent colors for flow types (fcode_desc)
 DEFAULT_FCODE_COLOR_MAP: dict[str, str] = {
-    "Artificial Path":             "#636EFA",
-    "Intermittent Stream / River": "#EF553B",
-    "Perennial Stream / River":    "#00CC96",
+    "Artificial Path": "#cf596b",
+    "Intermittent Stream / River": "#aaff00",
+    "Perennial Stream / River": "#004ca8",
+}
+
+DEFAULT_OWNER_COLOR_MAP: dict[str, str] = {
+    "Marine Corps": "#b2b2b2",
+    "Department of Agriculture": "#c8c8c8",
+    "Department of Veteran's Affairs": "#a8a8a8",
+    "Other Federal Agencies": "#d9d9d9",
+    "Army Corps of Engineers": "#9e9e9e",
+    "Fish and Wildlife Service": "#5bb3a6",
+    "National Park Service": "#d20ff0",
+    "State": "#75b3ff",
+    "Department of Energy": "#d8a388",
+    "Army Installations": "#c3c3c3",
+    "Private": "#e0e0e0",
+    "Local Government": "#bdd1ff",
+    "Bureau of Land Management": "#ffe98a",
+    "Bonneville Power Administration": "#d0d0d0",
+    "Undetermined": "#aca5a5",
+    "Air Force": "#bdbdbd",
+    "Bureau of Reclamation": "#1b007a",
+    "Department of Defense": "#fab3ff",
+    "Bureau of Prisons": "#8f8f8f",
+    "Forest Service": "#a1deb0",
+    "Federal Aviation Administration": "#bebebe",
+    "Bureau of Indian Affairs": "#ffb452",
+    "Navy Installations": "#a1a1a1",
 }
