36e674ae2da5718eee2d679524848d13919d0de6
Lorin Gaertner <lorin@northarrowresearch.com>
2026-09-15 16:43:46 -0700

    fixing copypasta in fargate (corrects an informational statement)

--- scripts/automation/fargate-rpt-project-context.sh
+++ scripts/automation/fargate-rpt-project-context.sh
@@ -52,7 +52,7 @@ try() {
     return 1
   fi
 
-  echo "======================  Running rpt-rivers-need-space ======================="
+  echo "======================  Running rpt-project-context ======================="
   python -m reports.rpt_project_context.main \
     "$OUTPUTS_DIR" \
     "$INPUTS_DIR/input.geojson" \
